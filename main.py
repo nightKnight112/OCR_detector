48ab37b92fe3ce5d4f0f4664f6560599ea083716
Archisman Das <archisman112358@gmail.com>
2021-10-16 11:11:21 +0530

    video part done, but very slow and inaccurate

--- main.py
+++ main.py
@@ -3,22 +3,30 @@ import sys
 import numpy as np
 import pytesseract.pytesseract as pyt
 
-#cap = cv2.VideoCapture(0)
-img = cv2.imread("Test_1.png")
+#print("Specify if OCR detection is to be applied on Video feed or Image")
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+#img = cv2.imread("Capture_1.PNG")
+
 pyt.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'  #req for versions > 3.05
 
-#charecter detection
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-h_img, w_img, _ = img.shape
+#charecter detection
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#h_img, w_img, _ = img.shape
 #print(h_img, w_img)
 
 #only charecter recognition
-data_obj = pyt.image_to_data(img, config=tessdata_dir_config)
+#data_obj = pyt.image_to_data(img, config=tessdata_dir_config)
 #print(data_obj)
 
-#individual charecter recognition part
+#individual charecter recognition
 '''
 for data in data_obj.splitlines():
     data = data.split(' ')
@@ -28,17 +36,37 @@ for data in data_obj.splitlines():
     cv2.putText(img, txt, (w-9, h_img-h), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
 '''
 
+'''
 #full word recognition part
 for count, data in enumerate(data_obj.splitlines()):
     if count != 0:
         data = data.split()
         if len(data) == 12:  #whole words come as lists with 12 elements
             txt, x, y, w, h = data[11], int(data[6]), int(data[7]), int(data[8]), int(data[9])
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 1)
-            cv2.putText(img, txt, (x - 9, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(img, txt, (x - 9, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
 
 cv2.imshow("final", img)
 
 if cv2.waitKey(0) or 0xff == ord('c'):
    sys.exit()
+'''
+while cap.isOpened():
+    ret, frm = cap.read()
+    frm = cv2.cvtColor(frm, cv2.COLOR_BGR2RGB)
+    h_img, w_img, _ = frm.shape
+    data_obj = pyt.image_to_data(frm, config=tessdata_dir_config)
+
+    for count, data in enumerate(data_obj.splitlines()):
+        if count != 0:
+            data = data.split()
+            if len(data) == 12:  # whole words come as lists with 12 elements
+                txt, x, y, w, h = data[11], int(data[6]), int(data[7]), int(data[8]), int(data[9])
+                cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(frm, txt, (x - 9, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow("frm_final", frm)
+
+    if cv2.waitKey(0) or 0xff == ord('c'):
+        sys.exit()
---
+++ video_OCR_part.py
@@ -0,0 +1,40 @@
+import cv2
+import keyboard as kb
+import numpy as np
+import pytesseract.pytesseract as pyt
+import sys
+from PIL import ImageGrab
+
+
+img = cv2.imread("Capture_1.PNG")
+pyt.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'  #req for versions > 3.05
+
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture("yt1s.com - Maroon 5  Memories Lyrics_480p.mp4")
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+while True:
+    success, img = cap.read()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imshow("Result_initial", img)
+
+    data_obj = pyt.image_to_data(img, config=tessdata_dir_config)
+    #print(data_obj)
+
+    for count, data in enumerate(data_obj.splitlines()):
+        if count != 0:
+            data = data.split()
+            if len(data) == 12:  # whole words come as lists with 12 elements
+                txt, x, y, w, h = data[11], int(data[6]), int(data[7]), int(data[8]), int(data[9])
+                print(txt)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(img, txt, (x - 9, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow("frm_final", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+sys.exit()
