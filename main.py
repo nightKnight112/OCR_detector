5ac931c7ed1519441d56816b1f876bbb56d598d0
Archisman Das <archisman112358@gmail.com>
2021-10-17 02:04:17 +0530

    screen capture tiling problem not fixed, rest issues fixed

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import sys
 import numpy as np
 import pytesseract.pytesseract as pyt
 from PIL import ImageGrab
+import pyautogui
 
-# basic setup
+#basic setup
 pyt.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'  # req for versions > 3.05
+tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'  #req for versions > 3.05
 frameWidth = 640
 frameHeight = 480
 
-
-# rescale and capture function for screen capture configuration
-def captureScreen(bbox=(50, 50, 1920, 1080)):
-    capScr = np.array(ImageGrab.grab(bbox))
+#rescale and capture function for screen capture configuration
+def captureScreen():
+    capScr = np.array(ImageGrab.grab(bbox=None))
     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
     return capScr
-
 
 def rescaleFrame(frame, scale):
     width = int(frame.shape[1] * scale)
@@ -24,22 +23,21 @@ def rescaleFrame(frame, scale):
     dimensions = (width, height)
     return cv2.resize(frame, dimensions)
 
-
 print("Specify if OCR detection is to be applied on Video feed or Image", "1. Still Image(enter 1)",
       "2. Video Feed(enter 2)", "3. Screen Capture(enter 3)", sep="\n")
 i = input()
-# print(i)
+#print(i)
 
 
-# for image feed
+#for image feed
 if int(i) == 1:
     print("Enter full image path: ")
     img = cv2.imread(input())
     print("to stop execution, press Q")
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    h_img, w_img, _ = img.shape
-    # print(h_img, w_img)
+    #h_img, w_img, _ = img.shape
+    #print(h_img, w_img)
 
     data_obj = pyt.image_to_data(img, config=tessdata_dir_config)
 
@@ -57,7 +55,7 @@ if int(i) == 1:
         sys.exit()
 
 
-# for video feed
+#for video feed
 elif int(i) == 2:
     print("Enter full video path: ")
     cap = cv2.VideoCapture(input())
@@ -75,7 +73,7 @@ elif int(i) == 2:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = cv2.filter2D(src=img, ddepth=-5, kernel=kernel)
 
-        # most probably not req this part
+        #most probably not req this part
         kernel_new = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
         img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel_new)
 
@@ -87,12 +85,12 @@ elif int(i) == 2:
 
         for count, data in enumerate(data_obj.splitlines()):
             if count != 0:
-                data = data.split()
-                if len(data) == 12:  # whole words come as lists with 12 elements
-                    txt, x, y, w, h = data[11], int(data[6]), int(data[7]), int(data[8]), int(data[9])
-                    print(txt)
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                    cv2.putText(img, txt, (x - 9, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                    data = data.split()
+                    if len(data) == 12:  # whole words come as lists with 12 elements
+                        txt, x, y, w, h = data[11], int(data[6]), int(data[7]), int(data[8]), int(data[9])
+                        #print(txt)
+                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        cv2.putText(img, txt, (x - 9, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         cv2.imshow("frm_final", img)
 
@@ -100,25 +98,44 @@ elif int(i) == 2:
             sys.exit()
 
 
-# for screen capture
+#for screen capture
 elif int(i) == 3:
     print("Starting screen capture, press Q to exit")
 
     # not completed from here onwards
-    # include tesseract and fix screen tiles
-    # remove fps counter
+    # fix screen tiles
     while True:
-        timer = cv2.getTickCount()
-        img = captureScreen()
-        img = rescaleFrame(img, 0.5)
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
-        cv2.putText(img, 'FPS {}'.format(int(fps)), (75, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (20, 230, 20), 2)
-        cv2.imshow('Screen Capture', img)
-        if cv2.waitKey(1) & 0xff == ord('q'):
-            sys.exit()
+            #timer = cv2.getTickCount()
+            #img = captureScreen()
+            #img = rescaleFrame(img, 0.5)
+            #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+            #cv2.putText(img, 'FPS {}'.format(int(fps)), (75, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (20, 230, 20), 2)
+
+            #new approach
+            img = pyautogui.screenshot()
+            frm = np.array(img)
+
+            cv2.imshow('Screen Capture', frm)
+            frm = cv2.cvtColor(frm, cv2.COLOR_BGR2RGB)
+
+            data_obj = pyt.image_to_data(frm, config=tessdata_dir_config)
+
+            for count, data in enumerate(data_obj.splitlines()):
+                if count != 0:
+                    data = data.split()
+                    if len(data) == 12:  # whole words come as lists with 12 elements
+                        txt, x, y, w, h = data[11], int(data[6]), int(data[7]), int(data[8]), int(data[9])
+                        #print(txt)
+                        cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        cv2.putText(frm, txt, (x - 9, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
+
+            cv2.imshow("frm_final", frm)
+
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                sys.exit()
 
 
-# execption case
+#execption case
 else:
     print("Unspecified input found, please rerun application again!!!")
     sys.exit()
